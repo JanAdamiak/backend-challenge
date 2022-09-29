cf918f8ee0b5505f6f8551d13fbd6898ef56d692
Edward Digby <ps@ejdigby.com>
2022-09-29 20:28:30 +0100

    Added v1 of challenge

---
+++ src/main.py
@@ -0,0 +1,15 @@
+def run():
+    """
+    Please update this function with a function that can run your 
+    solution end to end, producing an output file in the output/
+    directory.
+    """
+
+    print("Hello World")
+
+    return True
+
+
+
+if __name__ == "__main__":
+    run()
